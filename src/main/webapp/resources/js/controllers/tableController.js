8ec65c022555c9a2269a5c881dfed889d2e10d5d
kolr <kol-radik@yandex.ru>
2016-04-16 11:02:56 +0300

    workd and reloads cool

--- src/main/webapp/resources/js/controllers/tableController.js
+++ src/main/webapp/resources/js/controllers/tableController.js
@@ -5,7 +5,6 @@ app.factory("Word", function($resource){
 app.controller('TableController', ['$scope', '$http', "Word", function($scope, $http, Word) {
   $scope.language = 'english';
   $scope.words = [];
-  // debugger
   var url = function() {
     return {language: $scope.language||'english'};
   }
@@ -19,11 +18,15 @@ app.controller('TableController', ['$scope', '$http', "Word", function($scope, $
   };
 
   var updateOnDelete = function(res) {
-      if ($scope.words.length == 0 || res.data == "") {
-          $scope.words = Word.query(url());
-      } else {
-          $scope.words.push(res);
-      }
+      var deletedID = res.data;
+
+      var el = $scope.words.find(function(arrItem){
+          return arrItem.id === deletedID;
+      });
+
+      var deletedIndex = $scope.words.indexOf(el);
+
+      $scope.words = [...$scope.words.slice(0, deletedIndex), ...$scope.words.slice(deletedIndex + 1)];
   };
 
   update();
@@ -41,6 +44,6 @@ app.controller('TableController', ['$scope', '$http', "Word", function($scope, $
   $scope.deleteWord = function(id) {
       $http.delete("/" + $scope.language + "/phrases/" + id, function(){
           console.log(id + " was delete");
-      }).then(update);
+      }).then(updateOnDelete);
   };
 }]);
