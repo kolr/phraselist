e46aaf9221adfe07b88f61f3cb9e1f03b2271b20
kolr <kol-radik@yandex.ru>
2016-04-19 20:30:26 +0300

    Added Bean for storing data, that come from client.

---
+++ src/main/java/com/phraselist/model/beans/user/ClientUserBean.java
@@ -0,0 +1,57 @@
+package com.phraselist.model.beans.user;
+
+/**
+ * 19.04.2016
+ * Created by Rodion.
+ */
+public class ClientUserBean {
+    private String login;
+    private String email;
+    private String name;
+    private String lastname;
+    private String password;
+
+    public ClientUserBean() {
+        super();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
