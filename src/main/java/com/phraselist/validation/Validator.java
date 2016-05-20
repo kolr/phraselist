036f90f58b6c309aa84e54db300c066763225995
kolr <kol-radik@yandex.ru>
2016-05-20 20:20:24 +0300

    Added validator

---
+++ src/main/java/com/phraselist/validation/Validator.java
@@ -0,0 +1,27 @@
+package com.phraselist.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 20.05.2016
+ * Created by Rodion.
+ */
+public class Validator {
+    public static boolean loginValidation(String login) {
+        String loginTemplate = "[a-zA-Z0-9]*";
+        Pattern pattern = Pattern.compile(loginTemplate);
+        Matcher matcher = pattern.matcher(login);
+        return matcher.matches();
+    }
+
+    public static boolean passValidation(String pass) {
+        if (pass.length() >= 8) {
+            String loginTemplate = "[^!\"№;%:?*()_]*";
+            Pattern pattern = Pattern.compile(loginTemplate);
+            Matcher matcher = pattern.matcher(pass);
+            return matcher.matches();
+        }
+        return false;
+    }
+}
