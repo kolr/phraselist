da66cb2c4780b20772180572c921ebb8be519c25
kolr <kol-radik@yandex.ru>
2016-06-13 20:31:42 +0300

    Created Language Bean

---
+++ src/main/java/com/phraselist/model/beans/db/LanguageBean.java
@@ -0,0 +1,26 @@
+package com.phraselist.model.beans.db;
+
+/**
+ * 13.06.2016
+ * Created by Rodion.
+ */
+public class LanguageBean {
+    private long id;
+    private String language;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
